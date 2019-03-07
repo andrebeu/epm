73c1482c25ab88115ae536e4e04224c34bd8e4ec
andrebeu <aobeukers@gmail.com>
2019-03-07 14:00:53 -0500

    first version pureEM doing nback - d/n learn

--- nmback.py
+++ nmback.py
@@ -5,43 +5,51 @@ import numpy as np
 BATCH_SIZE = 1
 
 """ 
-output 0 for trials previous to N
+no preunroll: just backprop to begining
+make task_flag vectors non-randomizable and trainable
 """
 
-class NBackTask():
+class NMBackTask():
 
-  def __init__(self,nback):
-    self.nback = nback
+  def __init__(self,nback,mback,nstim):
+    """ assume m>n
+    """
+    self.nmbackL = [nback,mback]
+    self.nstim = nstim
     return None
 
-  def gen_seq(self,ntrials,nstim):
+  def gen_seq(self,ntrials,task_flag):
     """
+    task_flag can be 0 or 1 to indicate whether
+      to perform nback or mback.
     returns X=[[x(t),y(t-t)],...] Y=[[[y(t)]]]
         `batch,time,step`
     """
-    # ntrials += self.nback
-    seq = np.random.randint(0,nstim,ntrials)
-    # seq = np.arange(ntrials)
+    # sample either n or mback task
+    nmback = self.nmbackL[task_flag]
+    # compose sequnece
+    seq = np.random.randint(2,self.nstim+2,ntrials)
+    seq_roll = np.roll(seq,nmback)
     Xt = seq
-    Xroll = np.roll(seq,self.nback)
-    Yt = np.array(Xt == Xroll).astype(int)
-    X = np.expand_dims(Xt,0) 
-    Y = np.expand_dims(Yt,0)
+    Yt = (seq==seq_roll).astype(int)
+    Xt = np.append(task_flag,Xt)
+    Yt = np.append(task_flag,Yt)
+    X = np.expand_dims(Xt[:-1],0) 
+    Y = np.expand_dims(Yt[:-1],0)
     return X,Y
 
 
 class MetaLearner():
 
-  def __init__(self,stsize,depth,nback,nstim=3,embed_size=50):
+  def __init__(self,stsize,depth=None,nstim=3,edim=50):
     """
     """
     self.graph = tf.Graph()
     self.sess = tf.Session(graph=self.graph)
     self.stsize = stsize
-    self.depth = depth 
-    self.nback = nback
+    # self.depth = depth 
     self.nstim = nstim
-    self.embed_size = embed_size
+    self.edim = edim
     self.num_actions = 2
     self.build()
     return None
@@ -50,29 +58,30 @@ class MetaLearner():
     with self.graph.as_default():
       ## data feeding
       self.setup_placeholders()
-      self.xbatch_id,self.ybatch_id,self.itr_initop = self.data_pipeline() # x(batch,depth), y(batch,depth)
+      self.xbatch_id,self.ybatch_id,self.itr_initop = self.data_pipeline() 
       # embedding matrix and randomization op
-      self.emat = tf.get_variable('embedding_matrix',[self.nstim,self.embed_size],
-                    trainable=False,initializer=tf.initializers.random_normal(0,1)) 
-      self.randomize_emat = self.emat.initializer
+      self.stim_emat = tf.get_variable('stimulus_embedding_matrix',[self.nstim,self.edim],
+                        trainable=False,initializer=tf.initializers.random_normal(0,1)) 
+      self.task_emat = tf.get_variable('task_embedding_matrix',[2,self.edim],
+                        trainable=True,initializer=tf.initializers.random_normal(0,1)) 
+      self.emat = tf.concat([self.task_emat,self.stim_emat],0)
+      self.randomize_stim_emat = self.stim_emat.initializer
       ## inference
-      self.xbatch = tf.nn.embedding_lookup(self.emat,self.xbatch_id,name='xembed') # batch,depth,embsize
-      ##
-      self.yhat_unscaled_full,self.finalstate = self.RNNinference_keras(self.xbatch) # batch,depth,num_actions
-      # print('explicit unroll')
-      # self.yhat_unscaled_full,self.finalstate = self.RNNinference(self.xbatch) # batch,depth,num_actions
-      ## 
-      self.yhat_unscaled_bptt = self.yhat_unscaled_full[:,int(self.depth/2):,:] # batch,depth,num_actions
+      self.xbatch = tf.nn.embedding_lookup(self.emat,self.xbatch_id,name='xembed') 
+      self.yhat_unscaled,self.final_state = self.RNNinference_keras(self.xbatch) 
+      self.ybatch_onehot = tf.one_hot(self.ybatch_id,self.num_actions) 
       ## train
-      self.ybatch_onehot_full = tf.one_hot(self.ybatch_id,self.num_actions) # batch,depth,num_actions
-      self.ybatch_onehot_bptt = self.ybatch_onehot_full[:,int(self.depth/2):,:] # batch,depth,num_actions
       self.train_loss = tf.nn.softmax_cross_entropy_with_logits_v2(
-                          labels=self.ybatch_onehot_bptt,logits=self.yhat_unscaled_bptt)
+                          labels=self.ybatch_onehot,
+                          logits=self.yhat_unscaled)
       self.minimizer = tf.train.AdamOptimizer(0.005).minimize(self.train_loss)
       ## eval
-      self.yhat_sm = tf.nn.softmax(self.yhat_unscaled_full) # batch,depth,num_actions
-      self.eval_loss = tf.nn.softmax_cross_entropy_with_logits_v2(
-                          labels=self.ybatch_onehot_full,logits=self.yhat_unscaled_full)
+      self.yhat_sm = tf.nn.softmax(self.yhat_unscaled) 
+      # self.acc = tf.equal(self.ybatch_id,tf.argmax(self.yhat_sm,2,output_type=tf.int32))
+      # self.acc = tf.metrics.accuracy(
+      #             labels=self.ybatch_id,
+      #             predictions=tf.argmax(self.yhat_sm,2))
+      
       # other
       self.sess.run(tf.global_variables_initializer())
       self.saver_op = tf.train.Saver(max_to_keep=None)
@@ -102,37 +111,32 @@ class MetaLearner():
 
   def setup_placeholders(self):
     """
-    setsup placeholders as instance variables
+    setup placeholders as instance variables
     """
     self.xph = tf.placeholder(tf.int32,
-              shape=[None,self.depth],
+              shape=[None,None],
               name="xdata_placeholder")
     self.yph = tf.placeholder(tf.int32,
-                  shape=[None,self.depth],
+                  shape=[None,None],
                   name="ydata_placeholder")
     self.dropout_keep_pr = tf.placeholder(tf.float32,
                   shape=[],
                   name="dropout_ph")
-    self.cellstate_ph = tf.placeholder(tf.float32,
-                  shape=[None,self.stsize],
-                  name = "initialstate_ph")
     return None
 
   # inference
 
-  def RNNinference(self,xbatch):
+  def RNNinference_(self,xbatch):
     """ 
-    This explicitly unrolls the rnn on the input dimension
-    this is slower than using the _keras version of this method
-    but I am keeping it because the current implementation of 
-    the _keras version does not allow me to return the state trajectory
+    depricated. keeping this here in case I need to analyze state trajectory in the future
+    current keras implementation of RNN unroll does not allow outputting cell states
     """
     with tf.variable_scope('CELL_SCOPE') as cellscope:
       # setup RNN cell      
-      cell = tf.contrib.rnn.LayerNormBasicLSTMCell(
-                self.stsize,dropout_keep_prob=self.dropout_keep_pr)
+      # cell = tf.contrib.rnn.LayerNormBasicLSTMCell(
+      #       self.stsize,dropout_keep_prob=self.dropout_keep_pr)
       ## input projection
-      # [batch,depth,insteps,embed_size]
+      # [batch,depth,insteps,edim]
       xbatch = tf.layers.dense(xbatch,self.stsize,
                   activation=tf.nn.relu,name='inproj')
       # [batch,depth,insteps,stsize]
@@ -144,8 +148,8 @@ class MetaLearner():
         output,state = cell(xbatch[:,tstep,:], state)
         outputL.append(output)
     # output projection
-    outputs = tf.convert_to_tensor(outputL) # depth,batch,cellsize
-    outputs = tf.transpose(outputs,(1,0,2)) # batch,depth,cellsize
+    outputs = tf.convert_to_tensor(outputL) 
+    outputs = tf.transpose(outputs,(1,0,2)) 
     outputs = tf.layers.dense(outputs,self.stsize,
                   activation=tf.nn.relu,name='hidoutlayer')
     outputs = tf.layers.dense(outputs,self.num_actions,
@@ -154,9 +158,10 @@ class MetaLearner():
 
   def RNNinference_keras(self,xbatch):
     with tf.variable_scope('CELL_SCOPE') as cellscope:
-      ## setup architecture
       lstm_cell = tf.keras.layers.LSTMCell(self.stsize)
-      init_state = lstm_cell.get_initial_state(self.cellstate_ph)
+      init_state_var = tf.get_variable('init_state',[BATCH_SIZE,self.stsize],
+                        trainable=True,initializer=tf.initializers.random_normal(0,1)) 
+      init_state = lstm_cell.get_initial_state(init_state_var)
       lstm_layer = tf.keras.layers.RNN(
         lstm_cell,return_sequences=True,return_state=True)
       dropout_inlayer = tf.keras.layers.Dropout(rate=(1-self.dropout_keep_pr))
@@ -172,83 +177,75 @@ class MetaLearner():
     return outputs,final_state
 
 
+
+
 class Trainer():
 
-  def __init__(self,net):
-    self.task = NBackTask(net.nback)
+  def __init__(self,net,nback,mback,trials_per_episode=50):
+    self.trials_per_episode = trials_per_episode
     self.net = net
+    self.task = NMBackTask(nback,mback,nstim=self.net.nstim)
     return None
 
-  def train_step(self,Xdata,Ydata,cell_state):
+  def train_step(self,Xdata,Ydata,cell_state=None):
     feed_dict = { self.net.xph:Xdata,
                   self.net.yph:Ydata,
                   self.net.dropout_keep_pr:0.9,
-                  self.net.cellstate_ph:cell_state
                   }
     # initialize iterator
     self.net.sess.run([self.net.itr_initop],feed_dict)
     # update weights and compute final loss
     cell_st,_ = self.net.sess.run(
-      [self.net.finalstate,self.net.minimizer],feed_dict)
-    cell_st = cell_st[0] # only return c-state
-    return np.expand_dims(cell_st,0)
-    # return cell_st
+      [self.net.final_state,self.net.minimizer],feed_dict)
+    return cell_st
 
   def train_loop(self,num_epochs,epochs_per_session):
     """
     """
     num_evals = num_epochs
-    loss_arr = np.empty([num_evals])
     acc_arr = np.empty([num_evals])
     eval_idx = -1
     for ep_num in range(num_epochs):
       if ep_num%epochs_per_session == 0:
         # randomize embeddings
-        self.net.sess.run(self.net.randomize_emat)
-        # flush cell state
-        rand_cell_state = cell_state = np.random.randn(BATCH_SIZE,self.net.stsize)
+        self.net.sess.run(self.net.randomize_stim_emat)
       # # generate data
-      # emat = self.net.sess.run(self.net.emat)
-      Xdata,Ydata = self.task.gen_seq(self.net.depth,self.net.nstim)
+      task_flag = np.random.choice([0,1])
+      Xdata,Ydata = self.task.gen_seq(self.trials_per_episode,task_flag=task_flag)
       # train step
-      cell_state = self.train_step(Xdata,Ydata,cell_state)
+      self.train_step(Xdata,Ydata)
       # printing
       if ep_num%(num_epochs/num_evals) == 0:
         eval_idx += 1
-        evalstep_loss,evalstep_acc = self.eval_step(Xdata,Ydata,cell_state)
-        loss_arr[eval_idx] = np.mean(evalstep_loss)
+        evalstep_acc = self.eval_step(Xdata,Ydata)
         acc_arr[eval_idx] = np.mean(evalstep_acc)
         if ep_num%(num_epochs/20) == 0:
-          print(ep_num/num_epochs,np.mean(evalstep_loss)) 
-    return loss_arr,acc_arr
+          print(ep_num/num_epochs,np.mean(evalstep_acc)) 
+    return acc_arr
 
-  def eval_step(self,Xdata,Ydata,cell_state=None):
-    if type(cell_state) == type(None):
-      cell_state = np.random.randn(BATCH_SIZE,self.net.stsize)
+  def eval_step(self,Xdata,Ydata):
     ## setup
     feed_dict = { self.net.xph:Xdata,
                   self.net.yph:Ydata,
                   self.net.dropout_keep_pr:1.0,
-                  self.net.cellstate_ph:cell_state
                   }
     self.net.sess.run([self.net.itr_initop],feed_dict)
     ## eval
-    step_loss,step_yhat_sm,step_ybatch = self.net.sess.run(
-                                        [self.net.eval_loss,
+    step_yhat_sm,step_ybatch = self.net.sess.run(
+                                        [
                                         self.net.yhat_sm,
-                                        self.net.ybatch_onehot_full
+                                        self.net.ybatch_onehot
                                         ],feed_dict)
     step_acc = step_yhat_sm.argmax(2) == step_ybatch.argmax(2)
-    return step_loss,step_acc
+    return step_acc
 
-  def eval_loop(self,num_itr):
-    loss_arr = np.empty([num_itr,self.net.depth])
-    acc_arr = np.empty([num_itr,self.net.depth])
+  def eval_loop(self,num_itr,trials_per_episode=None,task_flag=None):
+    if trials_per_episode==None:
+      trials_per_episode = self.trials_per_episode
+    acc_arr = np.zeros([num_itr,trials_per_episode])
     for it in range(num_itr):
-      self.net.sess.run(self.net.randomize_emat)
-      rand_cell_state = cell_state = np.random.randn(BATCH_SIZE,self.net.stsize)
-      Xdata,Ydata = self.task.gen_seq(self.net.depth,self.net.nstim)
-      step_loss,step_acc = self.eval_step(Xdata,Ydata,rand_cell_state)
-      loss_arr[it] = step_loss
+      self.net.sess.run(self.net.randomize_stim_emat)
+      Xdata,Ydata = self.task.gen_seq(trials_per_episode,task_flag)
+      step_acc = self.eval_step(Xdata,Ydata)
       acc_arr[it] = step_acc
-    return loss_arr,acc_arr
+    return acc_arr
---
+++ pureEM.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+
+
+BATCH_SIZE = 1
+
+class PureEM():
+
+  def __init__(self,ntrials,nstim,dim):
+    self.ntrials = ntrials
+    self.nstim = nstim
+    self.dim = dim
+    self.graph = tf.Graph()
+    self.sess = tf.Session(graph=self.graph)
+    self.build()
+    return None
+
+  def build(self):
+    with self.graph.as_default():
+      ## model inputs
+      self.trial_ph,self.stim_ph,self.y_ph = self.setup_placeholders()
+      self.trial_embed,self.stim_embed = self.get_input_embeds()
+      ## compute internal state
+      internal_state = tf.keras.layers.Dense(self.dim,activation='sigmoid')(self.stim_embed)
+      ## Episodic memory [internal_state,trial_embed]
+      self.M_keys,self.M_values = self.get_memory_mats()
+      # retrieve
+      self.retrieved_memory = retrieved_memory = self.retrieve_memory(internal_state)
+      # write {internal_state:trial_embed}
+      write_to_memory_value = tf.assign(
+                                self.M_values[tf.squeeze(self.trial_ph),:],
+                                tf.squeeze(internal_state))
+      write_to_memory_keys = tf.assign(
+                                self.M_keys[tf.squeeze(self.trial_ph),:],
+                                tf.squeeze(self.trial_embed))
+      self.write_to_memory = tf.group([write_to_memory_value,write_to_memory_keys])
+      # empty memory
+      zero_K = self.M_keys.initializer
+      zero_V = self.M_values.initializer
+      self.empty_memory = tf.group([zero_K,zero_V])
+      ## compute response
+      response_in = tf.concat([internal_state,retrieved_memory],axis=-1)
+      response_in = tf.keras.layers.Dense(self.dim,activation='sigmoid')(response_in)
+      response_logits = tf.keras.layers.Dense(2,activation=None)(response_in)
+      ## loss and optimization
+      self.train_loss = tf.nn.softmax_cross_entropy_with_logits_v2(
+                          labels=tf.one_hot(self.y_ph,2),
+                          logits=response_logits)
+      self.minimizer = tf.train.AdamOptimizer(0.05).minimize(self.train_loss)
+      ## eval
+      self.response_sm = tf.nn.softmax(response_logits)
+      self.response = tf.argmax(self.response_sm,axis=1)
+      ## extra
+      self.sess.run(tf.global_variables_initializer())
+    return None
+
+  def reinitialize(self):
+    """ reinitializes variables to reset weights"""
+    with self.graph.as_default():
+      self.sess.run(tf.global_variables_initializer())
+    return None
+
+  def retrieve_memory(self,query_key):
+    ## use internal state to retrieve memory
+    # compare internal state to stored internal states (keys)
+    state_Mkeys_sim = tf.keras.metrics.cosine(query_key,self.M_keys)
+    state_Mkeys_sim = tf.expand_dims(state_Mkeys_sim,axis=0)
+    state_Mkeys_sim = tf.nn.softmax(state_Mkeys_sim)
+    self.state_Mkeys_sim = state_Mkeys_sim
+    # use similarity to form memory retrieval
+    retrieved_memory = tf.transpose(
+                        tf.matmul(
+                          self.M_values,state_Mkeys_sim,
+                          transpose_a=True,transpose_b=True
+                        ))
+    return retrieved_memory
+
+  def setup_placeholders(self):
+    trial_ph = tf.placeholder(name='trial_ph',shape=[1],dtype=tf.int32)
+    stim_ph = tf.placeholder(name='stim_ph',shape=[1],dtype=tf.int32)
+    y_ph = tf.placeholder(name='true_y_ph',shape=[1],dtype=tf.int32)
+    return trial_ph,stim_ph,y_ph
+
+  def get_input_embeds(self):
+    # setupmat
+    self.trial_emat = trial_emat = tf.get_variable('trial_emat',[self.ntrials,self.dim],
+                    trainable=True,initializer=tf.initializers.identity()) 
+    stim_emat = tf.get_variable('stimulus_emat',[self.nstim,self.dim],
+                    trainable=True,initializer=tf.initializers.identity())
+    # lookup
+    trial_embed = tf.nn.embedding_lookup(trial_emat,self.trial_ph,name='trial_embed')
+    stim_embed = tf.nn.embedding_lookup(stim_emat,self.stim_ph,name='stim_embed')
+    return trial_embed,stim_embed
+
+
+  def get_memory_mats(self):
+    """ {internal_state: trial_embed}
+    """
+    M_keys = tf.get_variable('memory_matrix_keys',[self.ntrials,self.dim],
+                trainable=False,initializer=tf.initializers.zeros())
+    M_values = tf.get_variable('memory_matrix_values',[self.ntrials,self.dim],
+                trainable=False,initializer=tf.initializers.zeros())
+    return M_keys,M_values
